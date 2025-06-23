e8cfd326e0161fe22bce6de6e9e1a4eea9f22eab
Iniabasi Ekpenyong <iniek@Ini.(none)>
2025-06-23 11:06:05 -0400

    Potential fix

--- food/src/main.tsx
+++ food/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
 import App from './App.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-=======
-import Gal from './Gallery.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Gal />
->>>>>>> 10ffdef1c8f99e41d10670e45faf671170c5130c
   </StrictMode>,
 )
