8d07b906b28ffeafeeb4801b25a099915ea58d75
Iniabasi Ekpenyong <iniek@Ini.(none)>
2025-06-23 10:56:06 -0400

    Hyper Links

--- food/src/main.tsx
+++ food/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Thing from './Thingy.tsx'
+import App from './App.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Thing />
+    <App />
   </StrictMode>,
 )
