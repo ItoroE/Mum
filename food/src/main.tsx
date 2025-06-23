10ffdef1c8f99e41d10670e45faf671170c5130c
Itoro Ekpenyong <itorekpen@gmail.com>
2025-06-23 02:53:03 -0400

    changes to Gallery

--- food/src/main.tsx
+++ food/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Thing from './Thingy.tsx'
+import Gal from './Gallery.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Thing />
+    <Gal />
   </StrictMode>,
 )
